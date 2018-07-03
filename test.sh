25d4b85d46028ea5833064cfee887f914b8f8b65
Kai-Arne Reiter <git@mirrorspock.com>
2018-07-03 09:50:38 +0200

    added new testfiles

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ./sortfilekai.sh 20180629101718077_8785427650000_psi8lt6ubdk00000psi8lt6u_EDI_08A_M.edi
+./sortfilekai.sh 20180629101718077_8785427650000_psi8lt6ubdk00000psi8lt6u_EDI_08A_X.edi
 
 ./sortfilekai.sh 20180629104609567_315000554i_5cd5e5e8-90c9-4a33-a7bd-_IFTMIN_D99A_M.edi.gz
+./sortfilekai.sh 20180629104609567_315000554i_5cd5e5e8-90c9-4a33-a7bd-_IFTMIN_D99A_X.edi.gz
